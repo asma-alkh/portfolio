24a84ad465100a7a5344d8fb3e8dd4b189353227
asma2003 <asma200m2200@gmail.com>
2025-09-11 13:26:25 +0300

    mid day update

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,1 @@
-// Toggle menu
-const menuIcon = document.getElementById('bx-menu-icon');
-const navbar = document.querySelector('.navbar');
 
-menuIcon.addEventListener('click', () => {
-  navbar.classList.toggle('active');
-});
-
-// Close menu on link click
-document.querySelectorAll('.navbar a').forEach(link => {
-  link.addEventListener('click', () => {
-    navbar.classList.remove('active');
-  });
-});
